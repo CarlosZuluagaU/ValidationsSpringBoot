a1c97dc387f92ca1779777db7f63cc6a28cf9e45
Carlos Zuluaga <andres.zuluaga6@udea.edu.co>
2025-08-08 19:34:52 -0500

    adding a class with email configurations

--- src/main/java/com/app/advice/validation/validator/ValidEmailValidator.java
+++ src/main/java/com/app/advice/validation/validator/ValidEmailValidator.java
@@ -1,1 +1,21 @@
-git
+package com.app.advice.validation.validator;
+
+import com.app.advice.validation.anotation.ValidEmail;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ValidEmailValidator implements ConstraintValidator<ValidEmail, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        // Solo letras, números, puntos y guiones bajos antes del @
+        // Dominio solo letras, números y puntos, con extensión mínima de 2 letras
+        String emailRegex = "^[A-Za-z0-9._]+@[A-Za-z0-9.]+\\.[A-Za-z]{2,}$";
+
+        return value.matches(emailRegex);
+    }
+}
